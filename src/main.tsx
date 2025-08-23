f09974b82460d5bff3573675203a2c9ea956bdfe
Adrian Sandoval <adrian.sandoval@tcwglobal.com>
2025-08-23 11:30:01 -0700

    add sonner component

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { Toaster } from '@/components/ui/sonner'
+
 import '@/index.css'
 import { routeTree } from '@/lib/route-tree.gen'
 
@@ -16,5 +18,6 @@ declare module '@tanstack/react-router' {
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <RouterProvider basepath="/tug-of-war" router={router} />
+        <Toaster offset={24} toastOptions={{ classNames: { toast: 'right-0 w-[352px] border-2' } }} richColors />
     </StrictMode>
 )
